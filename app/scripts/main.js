9d231b1711b1426a52ce67b5ffe07fcd4613bc68
T Jay Carter <tjaycarter@T-Jay-Carters-Mac-Pro.local>
2015-11-29 21:20:16 -0700

    transferred code over
    Brought all the pieces over from the initial project. It’s working much
    better now.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+/*global $*/
+'use strict';
+
+$(function (){
+  // image stretches to fill container
+  $('.imageContainer').imagefill();
+
+  // breadcrumbs plugin
+  $('#breadcrumbs').rcrumbs();
+
+  //hide the rollover states
+  //$('#computerOver').hide();
+
+
+});
