e449ed5632897df9edc02d2d109ea19ad4806781
T Jay Carter <tjaycarter@MacPro.local>
2015-12-05 14:47:41 -0700

    finished POD html
    finished POD HTML and Styling
    working on jQuery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,13 +3,42 @@
 
 $(function (){
   // image stretches to fill container
-  $('.imageContainer').imagefill();
+  $('.imageContainer, .thumbnail, .videoThumb').imagefill();
 
   // breadcrumbs plugin
-  $('#breadcrumbs').rcrumbs();
+  //$('#breadcrumbs').rcrumbs();
+
+  // crude rollover functions as proof of concept
+
+  $('#computer').mouseenter(function(){
+    $('#computer').hide();
+    $('#computerOver').removeClass('under');
+  });
+
+  $('#people').mouseenter(function(){
+    $('#people').hide();
+    $('#peopleOver').removeClass('under');
+  });
+
+  $('#pod').mouseenter(function(){
+    $('#pod').hide();
+    $('#podOver').removeClass('under');
+  });
+
+  $('#digitalLibrary').mouseenter(function(){
+    $('#digitalLibrary').hide();
+    $('#digitalLibraryOver').removeClass('under');
+  });
 
   //hide the rollover states
   //$('#computerOver').hide();
 
-
+  //jQuery Socialist Plugin for social media feed
+  /*$('.socialFeed').socialist({
+        networks: [
+            {name:'facebook',id:'HighPlainsLibraryDistrict'},
+            {name:'twitter',id:'MyHPLD'},
+            {name:'pinterest',id:'hpldstuff'}
+        ]
+    });*/
 });
